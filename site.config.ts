84d6bc472342ccbb12b10632117ed926dac368d6
thirty、 <145795950+weihuangfei@users.noreply.github.com>
2024-12-02 00:55:39 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,7 @@ import { defineSiteConfig } from 'valaxy'
 export default defineSiteConfig({
   url: 'https://valaxy.site/',
   lang: 'zh-CN',
-  title: 'Valaxy Theme Yun',
+  title: 'thirty',
   author: {
     name: 'thirty',
   },
