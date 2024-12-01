54dc7dbf54f56f823bb1516a2f9bb5b607746a31
thirty、 <145795950+weihuangfei@users.noreply.github.com>
2024-12-02 00:54:24 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ export default defineSiteConfig({
   lang: 'zh-CN',
   title: 'Valaxy Theme Yun',
   author: {
-    name: '云游君',
+    name: 'thirty',
   },
   description: 'Valaxy Theme Yun Preview.',
   social: [
